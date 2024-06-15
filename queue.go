3f794bd8b6b69540e975d65488f95abe4caa4e10
Jason Frame <jwf@jasonframe.co.uk>
2024-06-15 16:47:32 +0100

    Initial

---
+++ queue.go
@@ -0,0 +1,90 @@
+package delayqueue
+
+import (
+	"container/heap"
+	"context"
+	"time"
+)
+
+type Queue[T any] struct {
+	C <-chan T
+
+	ctx       context.Context
+	ch        chan T
+	additions chan item[T]
+	items     pqueue[T]
+}
+
+func New[T any](ctx context.Context, outBufferSize int) *Queue[T] {
+	out := &Queue[T]{
+		ctx:       ctx,
+		ch:        make(chan T, outBufferSize),
+		additions: make(chan item[T]),
+		items:     make(pqueue[T], 0),
+	}
+	out.C = out.ch
+	return out
+}
+
+func (q *Queue[T]) Run() {
+	timer := time.NewTimer(0)
+	if !timer.Stop() {
+		<-timer.C
+	}
+
+	var nextDueAt time.Time
+
+	for {
+		select {
+		case i := <-q.additions:
+			heap.Push(&q.items, i)
+			if nextDueAt.IsZero() || i.Due.Before(nextDueAt) {
+				if !nextDueAt.IsZero() && !timer.Stop() {
+					<-timer.C
+				}
+				nextDueAt = i.Due
+				timer.Reset(time.Until(nextDueAt))
+			}
+		case <-timer.C:
+			out := heap.Pop(&q.items).(item[T])
+			if q.items.Len() > 0 {
+				nextDueAt = q.items[0].Due
+				timer.Reset(time.Until(nextDueAt))
+			} else {
+				nextDueAt = time.Time{}
+			}
+			q.ch <- out.V
+		case <-q.ctx.Done():
+			return
+		}
+	}
+}
+
+func (q *Queue[T]) Add(due time.Time, val T) error {
+	select {
+	case <-q.ctx.Done():
+		return q.ctx.Err()
+	case q.additions <- item[T]{Due: due, V: val}:
+		return nil
+	}
+}
+
+type item[T any] struct {
+	Due time.Time
+	V   T
+}
+
+type pqueue[T any] []item[T]
+
+func (q pqueue[T]) Len() int           { return len(q) }
+func (q pqueue[T]) Less(i, j int) bool { return q[i].Due.Before(q[j].Due) }
+func (q pqueue[T]) Swap(i, j int)      { q[i], q[j] = q[j], q[i] }
+func (q *pqueue[T]) Push(x any)        { *q = append(*q, x.(item[T])) }
+
+func (q *pqueue[T]) Pop() any {
+	old := *q
+	n := len(old)
+	x := old[n-1]
+	*q = old[0 : n-1]
+	return x
+}
---
+++ queue_test.go
@@ -0,0 +1,59 @@
+package delayqueue
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestElementsYieldedInTimeOrder(t *testing.T) {
+	q := New[int](context.Background(), 3)
+	go q.Run()
+
+	q.Add(time.Now().Add(100*time.Millisecond), 2)
+	q.Add(time.Now().Add(200*time.Millisecond), 3)
+	q.Add(time.Now().Add(50*time.Millisecond), 1)
+
+	collect := []int{}
+	for i := 0; i < 3; i++ {
+		collect = append(collect, <-q.C)
+	}
+
+	assert.Equal(t, 1, collect[0])
+	assert.Equal(t, 2, collect[1])
+	assert.Equal(t, 3, collect[2])
+}
+
+const epsilon = 5 * time.Millisecond
+
+func TestElementsYieldedAtCorrectTime(t *testing.T) {
+	now := time.Now()
+
+	vals := []int{1, 2, 3, 4, 5}
+	dues := []time.Time{
+		now.Add(100 * time.Millisecond),
+		now.Add(300 * time.Millisecond),
+		now.Add(600 * time.Millisecond),
+		now.Add(650 * time.Millisecond),
+		now.Add(1 * time.Second),
+	}
+
+	q := New[int](context.Background(), 5)
+	go q.Run()
+
+	for i := range vals {
+		q.Add(dues[i], vals[i])
+	}
+
+	for i := range vals {
+		v := <-q.C
+		assert.Equal(t, vals[i], v)
+		dt := time.Since(dues[i])
+		if dt < 0 {
+			dt = -dt
+		}
+		assert.True(t, dt < epsilon)
+	}
+}
